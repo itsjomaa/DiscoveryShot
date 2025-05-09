8e8b66fdc01bd11dc761f6e892c5dfc73a8b1d8f
Jomari <jomaaed@icloud.com>
2025-05-09 17:20:17 +0200

    changes to solve

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-document.getElementById('promo-video').addEventListener('ended', function () {
-    document.querySelector('.video-overlay').style.opacity = '1';
-});
+
 
 document.querySelector('.menu-btn').addEventListener('click', function () {
     this.classList.toggle('active');
@@ -14,12 +12,6 @@ document.addEventListener('click', function (e) {
         document.querySelector('.sidebar').classList.remove('active');
     }
 });
-
-
-// Run the function when the DOM is fully loaded
-document.addEventListener('DOMContentLoaded', updateAgeRange);
-// Update the age range every year (you can remove this if you only want it to update on page load)
-setInterval(updateAgeRange, 365 * 24 * 60 * 60 * 1000);
 
 document.addEventListener('DOMContentLoaded', function () {
     const video = document.getElementById('player');
