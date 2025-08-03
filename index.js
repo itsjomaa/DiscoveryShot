05e9587764dc4c2dd406ea7c1e54a1ae47300c39
Jomari <jomaaed@icloud.com>
2025-08-03 15:25:24 +0200

    animation

--- index.js
+++ index.js
@@ -60,3 +60,26 @@ document.addEventListener('DOMContentLoaded', function () {
         updateSlider();
     });
 });
+
+// Scroll Animation
+document.addEventListener('DOMContentLoaded', function () {
+    // Initial check for elements in viewport on page load
+    checkReveal();
+
+    // Check for elements in viewport on scroll
+    window.addEventListener('scroll', checkReveal);
+
+    function checkReveal() {
+        const reveals = document.querySelectorAll('.reveal');
+
+        for (let i = 0; i < reveals.length; i++) {
+            const windowHeight = window.innerHeight;
+            const revealTop = reveals[i].getBoundingClientRect().top;
+            const revealPoint = 150; // Adjust this value to change when the animation triggers
+
+            if (revealTop < windowHeight - revealPoint) {
+                reveals[i].classList.add('active');
+            }
+        }
+    }
+});
